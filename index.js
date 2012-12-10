9e7d3095e9426087f7a23ecfe93e13f0e8d1b5af
Brian McKenna <brian@precog.com>
2012-12-10 19:00:36 +1000

    Make library work with assignments of function expressions
    Now allows:
    
        var count = function(from, to) {
            if(from >= to)
                return from;
    
            return count(from + 1, to);
        }
    
        console.log(count(0, 1000000));
    
    And:
    
        var count;
        count = function(from, to) {
            if(from >= to)
                return from;
    
            return count(from + 1, to);
        }
    
        console.log(count(0, 1000000));
    
    Uses estraverse instead of traverse

--- index.js
+++ index.js
@@ -8,25 +8,71 @@ var estraverse = require('estraverse'),
         name: '__tcor'
     };
 
-function returnValue(r) {
-    this.update({
-        type: 'BlockStatement',
-        body: [{
-            type: 'ExpressionStatement',
-            expression: {
-                type: 'AssignmentExpression',
-                operator: '=',
-                left: resultIdentifier,
-                right: r.argument
-            }
-        }, {
-            type: 'BreakStatement',
-            label: tcoLabel
-        }]
+function equals(a, b) {
+    var equal = true,
+        k;
+
+    if(a === null || b === null)
+        return false;
+
+    if(a == b)
+        return true;
+
+    for(k in a) {
+        equal = equal && equals(a[k], b[k]);
+    }
+
+    for(k in b) {
+        equal = equal && equals(a[k], b[k]);
+    }
+
+    return equal;
+}
+
+function traverseWithStack(t, o) {
+    var stack = [];
+    estraverse.traverse(t, {
+        enter: function(n) {
+            stack.unshift(n);
+            if(o.enter) o.enter(n, stack);
+        },
+        leave: function(n) {
+            stack.shift();
+            if(o.leave) o.leave(n, stack);
+        }
     });
 }
 
-function tailCall(f, r) {
+function nodeStack(top, f) {
+    var result = [];
+    traverseWithStack(top, {
+        enter: function(n, stack) {
+            if(n != f)
+                return;
+
+            result = stack.slice();
+        }
+    });
+    return result;
+}
+
+function returnValue(r, stack) {
+    r.type =  'BlockStatement';
+    r.body = [{
+        type: 'ExpressionStatement',
+        expression: {
+            type: 'AssignmentExpression',
+            operator: '=',
+            left: resultIdentifier,
+            right: r.argument
+        }
+    }, {
+        type: 'BreakStatement',
+        label: tcoLabel
+    }];
+}
+
+function tailCall(f, r, stack) {
     var tmpVars = [],
         assignments = [],
         i,
@@ -53,33 +99,36 @@ function tailCall(f, r) {
         });
     }
 
-    this.update({
-        type: 'BlockStatement',
-        body: [{
-            type: 'VariableDeclaration',
-            declarations: tmpVars,
-            kind: 'var'
-        }].concat(assignments).concat({
-            type: 'ContinueStatement',
-            label: tcoLabel
-        })
+    r.type = 'BlockStatement';
+    r.body = [{
+        type: 'VariableDeclaration',
+        declarations: tmpVars,
+        kind: 'var'
+    }].concat(assignments).concat({
+        type: 'ContinueStatement',
+        label: tcoLabel
     });
 }
 
-function optimizeFunction(f) {
-    var name = f.id.name,
-        block = f.body;
+function optimizeFunction(top, f) {
+    var block = f.body,
+        traversal = {
+            enter: function(n, stack) {
+                if(n.type != 'ReturnStatement')
+                    return;
 
-    traverse(block.body).forEach(function(n) {
-        if(!n || n.type != 'ReturnStatement')
-            return;
+                if(n.argument.type == 'CallExpression' && equals(n.argument.callee, functionId(top, f))) {
+                    tailCall(f, n, stack);
+                } else {
+                    returnValue(n, stack);
+                }
+            }
+        },
+        i;
 
-        if(n.argument.type == 'CallExpression' && n.argument.callee.name == name) {
-            tailCall.call(this, f, n);
-        } else {
-            returnValue.call(this, n);
-        }
-    });
+    for(i = 0; i < block.body.length; i++) {
+        traverseWithStack(block.body[i], traversal);
+    }
 
     block.body = [{
         type: 'VariableDeclaration',
@@ -108,53 +157,65 @@ function optimizeFunction(f) {
     }];
 }
 
-function topLevel(f, n) {
-    var name = f.id.name,
-        parent = n;
+function topLevel(top, n) {
+    var stack = nodeStack(top, n),
+        i;
 
-    while(parent) {
-        if(parent.node.type == 'FunctionExpression') {
-            return false;
-        }
-
-        if(parent.node.type == 'FunctionDeclaration') {
-            if(parent.node.id.name == name) {
-                return true;
-            } else {
-                return false;
+    for(i = 0; i < stack.length; i++) {
+        if(stack[i].type == 'FunctionExpression') {
+            if(stack[i + 1].type == 'VariableDeclarator') {
+                return equals(stack[i + 1].id, n.callee);
+            } else if(stack[i + 1].type == 'AssignmentExpression') {
+                return equals(stack[i + 1].left, n.callee);
             }
+            return false;
+        } else if(stack[i].type == 'FunctionDeclaration') {
+            return equals(stack[i].id, n.callee);
         }
-
-        parent = parent.parent;
     }
-
-    return false;
 }
 
-function hasOnlyTailCalls(f) {
-    var name = f.id.name,
-        result = traverse(f).reduce(function(accum, n) {
-            if(!accum.all || !n || n.type != 'CallExpression' || n.callee.name != name)
-                return accum;
+function functionId(top, f) {
+    var stack;
+    if(f.type == 'FunctionDeclaration') {
+        return f.id;
+    }
 
-            return {
-                any: true,
-                all: accum.all && this.parent.node.type == 'ReturnStatement' && topLevel(f, this)
-            };
-        }, {
-            any: false,
-            all: true
+    stack = nodeStack(top, f);
+    if(stack[1].type == 'VariableDeclarator') {
+        return stack[1].id;
+    } else {
+        return stack[1].left;
+    }
+}
+
+function hasOnlyTailCalls(top, f) {
+    var all = true,
+        any = false,
+        result = traverseWithStack(f, {
+            enter: function(n, stack) {
+                if(!all || n.type != 'CallExpression')
+                    return;
+
+                if(!equals(n.callee, functionId(top, f)))
+                    return;
+
+                any = true;
+                all = all && stack[1].type == 'ReturnStatement' && topLevel(top, n);
+            }
         });
 
-    return result.any && result.all;
+    return any && all;
 }
 
 function mutateAST(ast) {
-    traverse(ast).forEach(function(n) {
-        if(!n || n.type != 'FunctionDeclaration' || !hasOnlyTailCalls(n))
-            return;
+    estraverse.traverse(ast, {
+        enter: function(n) {
+            if(['FunctionDeclaration', 'FunctionExpression'].indexOf(n.type) == -1 || !hasOnlyTailCalls(ast, n))
+                return;
 
-        optimizeFunction(n);
+            optimizeFunction(ast, n);
+        }
     });
 }
 
